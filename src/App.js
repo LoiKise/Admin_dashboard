851dc785e3e421e43fe4e31b6c1b65a1fa4a8a49
hoainho <nhoxt@DESKTOP-HSIS64O>
2021-11-25 22:55:49 +0700

    add dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Chart from "./Template/Chart";
-import Header from "./Layouts/Header";
 import Home from "./Template/Home/Home"
 import Login from "./Template/Login/Login";
-
+import AdminPage from "./components/Admin/Dashboard/Dashboard";
+import AdminScreen from "./Template/Admin";
 function App() {
 	return (
 		<BrowserRouter>
-      <Switch>
-       
+			<Switch>
+
+				<AdminScreen exact path="/admin/dashboard" Component={AdminPage} />
 				<Home exact path="/home" Component={Chart} />
-        <Route exact path="/login" component={Login} />
-        <Home exact path="/" Component={Chart} />
+				<Route exact path="/login" component={Login} />
+				<Home exact path="/" Component={Chart} />
 			</Switch>
 		</BrowserRouter>
 	);
